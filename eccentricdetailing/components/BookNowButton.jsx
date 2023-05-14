dfc87902b24f86b42a9471c6fb68d60a1530831e
notzree <notzree@gmail.com>
2023-05-14 14:26:40 -0400

    edited detailing page

---
+++ eccentricdetailing/components/BookNowButton.jsx
@@ -0,0 +1,6 @@
+export default function BookButton(){
+
+    return(
+        <a className="btn" href="https://squareup.com/appointments/book/xzx7iluifnx3n8/L95D8Y2HQSHR0/services">Book now</a>
+    )
+}
--- eccentricdetailing/pages/detailing.js
+++ eccentricdetailing/pages/detailing.js
@@ -1,3 +1,4 @@
+import BookButton from "@/components/BookNowButton";
 import Footer from "@/components/Footer";
 import Header from "@/components/header";
 
@@ -11,15 +12,38 @@ export default function Detailing() {
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center">
     <div className="">
-      <h1 className="mb-5 text-5xl font-bold">Detailing</h1>
+      <h1 className="mb-5 text-5xl font-bold">Vehicle Detailing</h1>
       <p className="mb-5 w-auto">
-        Experience exceptional car detailing with unparalleled attention to detail</p>
-      <button className="btn ">Book now</button>
+        Experience exceptional vehicle detailing with unparalleled attention to detail</p>
+      <BookButton/>
     </div>
   </div>
 </div>
-<div className="h-screen bg-blue-50">
-penis
+<div className="h-screen bg-blue-50 px-10">
+    
+        <h1 className="mb-5 text-5xl font-bold pt-2">What do we do?</h1>
+        <div className="flex md:flex-row flex-col h-1/4 gap-4  justify-between">
+            <div className="w-[20rem] justify-stretch items-center flex ">
+            <p className="inline w-full">
+            Insert description heread
+            ihefihaewofhewoifuhaiuehaofiheia
+            ofuheawoifuhawioeufhaewoifuhawef
+        </p>
+        </div>
+        <div className="flex items-center ">
+            <img src="https://www.topgear.com/sites/default/files/2022/10/field-adro-g8x-m3-m4-green-front-q-2.jpg" alt="" className="  h-auto md:h-[19rem] rounded-xl opacity-80"/>
+        </div>
+        </div>
+        <div className="h-[2/3vh] flex flex-col justify-center items-center gap-4 pt-6">
+        <h1 className="mb-5 text-5xl font-bold pt-2">Our packages</h1>
+        <div className="flex items-center justify-center">
+        
+        </div>
+        </div>
+        
+        
+    
+
 </div>
             <Footer/>
         </>
