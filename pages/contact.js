48a5f2d1532e8d18f0a0ee889940d2ebab8248e1
notzree <notzree@gmail.com>
2023-07-08 18:02:37 -0400

    updaded photos

--- pages/contact.js
+++ pages/contact.js
@@ -1,17 +1,4 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
+
 import { useState } from "react";
 import { useEffect } from "react";
 import Link from "next/link";
