7460d658c6d82c05bf6e410728d95ceccaf99932
Annie Cai <caishajia@gmail.com>
2023-07-08 19:19:56 -0400

    fixed packages for detailing

--- pages/detailing.js
+++ pages/detailing.js
@@ -5,21 +5,21 @@ import ViewPortfolio from "@/components/ViewPortfolio";
 import { CheckIcon } from '@heroicons/react/20/solid'
 
 const EliteincludedFeatures = [
-  'Exterior Hand wash and dry',
+  'Exterior hand wash and dry',
   'Exterior clay bar and premium wax',
   'Interior vacuum and trash removal',
   'Interior carpet shampoo',
   'Interior steam and whip down',
-  'Wheel Clean and detailing',
-  'Leather Conditioning',
+  'Wheel clean and detailing',
+  'Leather conditioning',
   'Window cleaning',
   'Engine bay'
 ]
 const PrestigeincludedFeatures = [
-  'Exterior Hand wash and dry',
+  'Exterior hand wash and dry',
   'Interior vacuum and trash removal',
   'Interior carpet shampoo',
-  'Wheel Clean and detailing',
+  'Wheel clean and detailing',
   'Window cleaning',
 ]
 
@@ -29,7 +29,7 @@ export default function Detailing() {
       <Header />
 
 
-      <div className="hero min-h-screen" style={{"background-image": "url(/detailingHeroPhoto.png)"}}>
+      <div className="hero min-h-screen" style={{ "background-image": "url(/detailingHeroPhoto.png)" }}>
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center">
           <div className="">
@@ -40,43 +40,37 @@ export default function Detailing() {
           </div>
         </div>
       </div>
-      <div className='w-screen flex flex-col md:flex-row'>
-        <div className="isolate bg-white px-6 py-20 lg:px-8 md:w-1/2">
+      <div className='w-screen'>
+        <div className="isolate bg-white py-20 lg:px-8">
           <div
             className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
             aria-hidden="true"
           >
-
           </div>
-          <div className="mx-auto max-w-2xl text-left">
+          <div className="p-12">
             <h2 className="text-3xl font-bold tracking-tight text-black sm:text-4xl">WHAT WE DO</h2>
-            <div className="mx-auto mt-5 max-w-xl sm:mt-20 sm:text-lg text-xs">
-            Interior detailing focuses on deep cleaning and restoring the various components inside the car. This includes vacuuming the seats, carpets, and floor mats to remove dirt and debris. Leather surfaces are carefully cleaned and conditioned to revive their original look and feel. The dashboard, center console, door panels, and other interior surfaces are cleaned and treated with suitable products to remove dust, stains, and marks. 
-<br></br>
-<br></br>
-Exterior detailing begins with a wheel cleanse using tire shine and shampoo to give a glossy look. We then apply a comprehensive wash to remove dirt, grime, and contaminants. This is then followed by a foam cannon, hand wash, and gentle towel dry to remove stains from the surface. 
-<br></br>
-<br></br>
-Additional steps can be taken depending on the detailing package and specific needs of the vehicle. These may include engine bay cleaning, headlight restoration, and more.
+            <div className="mt-8 sm:text-base text-xs">
+              Interior detailing focuses on deep cleaning and restoring the various components inside the car. This includes vacuuming the seats, carpets, and floor mats to remove dirt and debris. Leather surfaces are carefully cleaned and conditioned to revive their original look and feel. The dashboard, center console, door panels, and other interior surfaces are cleaned and treated with suitable products to remove dust, stains, and marks.
+              <br></br>
+              <br></br>
+              Exterior detailing begins with a wheel cleanse using tire shine and shampoo to give a glossy look. We then apply a comprehensive wash to remove dirt, grime, and contaminants. This is then followed by a foam cannon, hand wash, and gentle towel dry to remove stains from the surface.
+              <br></br>
+              <br></br>
+              Additional steps can be taken depending on the detailing package and specific needs of the vehicle. These may include engine bay cleaning, headlight restoration, and more.
 
             </div>
           </div>
-        </div>
-        <div className="md:w-1/2 bg-cover" style={{ backgroundImage: `url("https://www.topgear.com/sites/default/files/2022/10/field-adro-g8x-m3-m4-green-front-q-2.jpg")` }}>
-
         </div>
       </div>
       <div className="bg-black py-20 sm:py-25">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl sm:text-center">
             <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">OUR PACKAGES</h2>
-            <p className="mt-6 text-lg leading-8 text-white">
-              3 Different packages based off vehicle size and condition.
-            </p>
+
           </div>
           <div className="pt-10 flex justify-center items-center"><BookButton /></div>
-          
-          <div className="mx-auto mt-16 max-w-2xl border-2 rounded-xl ring-1 ring-white sm:mt-20 lg:mx-0 lg:flex lg:max-w-none mb-4 sm:mb-0">
+
+          <div className="mx-10 mt-16 border-2 rounded-xl ring-1 ring-white sm:mt-20 lg:flex mb-4 sm:mb-0">
             <div className="p-8 sm:p-10 lg:flex-auto">
               <h3 className="text-2xl font-bold tracking-tight text-white">Prestige Detailing</h3>
               <p className="mt-4 text-base leading-7 text-white">
@@ -121,16 +115,16 @@ Additional steps can be taken depending on the detailing package and specific ne
                   <p className="mt-6 text-xs leading-5 text-gray-600">
                     Vehicle sizing up to Eccentric's discretion. Add ons available.
                   </p>
-                
+
                 </div>
               </div>
             </div>
           </div>
-          <div className="mx-auto mt-0 max-w-2xl border-2 rounded-xl ring-1 ring-white sm:mt-10 lg:mx-0 lg:flex lg:max-w-none">
+          <div className="mx-10 mt-16 border-2 rounded-xl ring-1 ring-white sm:mt-20 lg:flex mb-4 sm:mb-0">
             <div className="p-8 sm:p-10 lg:flex-auto">
               <h3 className="text-2xl font-bold tracking-tight text-white">Elite Detailing</h3>
               <p className="mt-4 text-base leading-7 text-white">
-                Our Elite Detailing package is for cars that need a little more love, our most pouplar level package and by far most loved for the price to value from our customers. 
+                Our Elite Detailing package is for cars that need a little more love, our most pouplar level package and by far most loved for the price to value from our customers.
               </p>
               <div className="mt-5 flex items-center gap-x-4">
                 <h4 className="flex-none text-sm font-semibold leading-6 text-neutral-500 ">What’s included</h4>
@@ -149,7 +143,7 @@ Additional steps can be taken depending on the detailing package and specific ne
               </ul>
             </div>
             <div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
-              <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
+              <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-28">
                 <div className="mx-auto max-w-xs px-8">
                   <p className=" mt-6 text-base font-semibold text-neutral-500">Small vehicle</p>
                   <p className=" flex items-baseline justify-center gap-x-2">
@@ -168,14 +162,14 @@ Additional steps can be taken depending on the detailing package and specific ne
                   </p>
 
                   <p className="mt-6 text-xs leading-5 text-gray-600">
-                  Vehicle sizing up to Eccentric's discretion. Add ons available.
+                    Vehicle sizing up to Eccentric's discretion. Add ons available.
                   </p>
                 </div>
               </div>
             </div>
-            
+
           </div>
-          <div className="mx-auto mt-16 max-w-2xl border-2 rounded-xl ring-1 ring-white sm:mt-20 lg:mx-0 lg:flex lg:max-w-none mb-4 sm:mb-0">
+          <div className="mx-10 mt-16 border-2 rounded-xl ring-1 ring-white sm:mt-20 lg:flex mb-4 sm:mb-0">
             <div className="p-8 sm:p-10 lg:flex-auto">
               <h3 className="text-2xl font-bold tracking-tight text-white">Ultimate Detailing</h3>
               <p className="mt-4 text-base leading-7 text-white">
@@ -203,31 +197,31 @@ Additional steps can be taken depending on the detailing package and specific ne
                 <div className="mx-auto max-w-xs px-8">
                   <p className="text-base font-semibold text-neutral-500">Small vehicles</p>
                   <p className=" flex items-baseline justify-center gap-x-2">
-                    <span className="text-5xl font-bold tracking-tight text-gray-900">$145</span>
+                    <span className="text-4xl sm:text-5xl font-bold tracking-tight text-gray-900">$145</span>
                     <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">CAD</span>
                   </p>
                   <p className="mt-6 text-base font-semibold text-neutral-500">Medium vehicles</p>
                   <p className=" flex items-baseline justify-center gap-x-2">
-                    <span className="text-5xl font-bold tracking-tight text-gray-900">$155</span>
+                    <span className="text-4xl sm:text-5xl font-bold tracking-tight text-gray-900">$155</span>
                     <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">CAD</span>
                   </p>
                   <p className=" mt-6 text-base font-semibold text-neutral-500">Large vehicles (3 rows)</p>
                   <p className=" flex items-baseline justify-center gap-x-2">
-                    <span className="text-5xl font-bold tracking-tight text-gray-900">$180</span>
+                    <span className="text-4xl sm:text-5xl font-bold tracking-tight text-gray-900">$180</span>
                     <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">CAD</span>
                   </p>
 
                   <p className="mt-6 text-xs leading-5 text-gray-600">
                     Vehicle sizing up to Eccentric's discretion. Add ons available.
                   </p>
-                
+
                 </div>
               </div>
             </div>
           </div>
-        
+
         </div>
-        
+
 
       </div>
       <ViewPortfolio />
